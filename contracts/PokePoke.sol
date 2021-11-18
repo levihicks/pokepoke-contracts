14b47b9a460ce510309734d5b1797ab13024de38
levi hicks <lv.hicks@gmail.com>
2021-11-17 20:37:28 -0600

    add event for poking, error for self-poking, and additional tests

--- contracts/PokePoke.sol
+++ contracts/PokePoke.sol
@@ -11,12 +11,25 @@ contract PokePoke {
         address pokedBy; // the sender of the poke
     }
 
+    event PokeEvent (
+        address indexed to,
+        Poke pokeData
+    );
+
+    /// Self-poking not allowed.
+    error SelfPokeAttempted();
+
     mapping(address => Poke[]) public pokes;
     
     /// @notice Pokes an account
     /// @param _pokeRecipient The recipient of the poke
     function poke(address _pokeRecipient) public {
-        pokes[_pokeRecipient].push(Poke(block.timestamp, msg.sender));
+        if (msg.sender == _pokeRecipient)
+          revert SelfPokeAttempted();
+
+        Poke memory pokeData = Poke(block.timestamp, msg.sender);
+        pokes[_pokeRecipient].push(pokeData);
+        emit PokeEvent(_pokeRecipient, pokeData);
     }
     
     /// @notice Returns array of pokes received for an address
--- test/PokePoke.test.js
+++ test/PokePoke.test.js
@@ -11,4 +11,20 @@ contract("PokePoke", (accounts) => {
         const recipientPokes = await instance.viewPokes(accounts[1]);
         assert.equal(recipientPokes[0].pokedBy, accounts[0], "The sender of the poke should be shown in the recipient's list of pokes.");
     });
+
+    it("emits event properly upon poke", async () => {
+        const tx = await instance.poke(accounts[1], { from: accounts[0] });
+        assert.equal(tx.logs[0].event, 'PokeEvent', "A PokeEvent should be emitted.");
+    });
+
+    it("triggers error when self-poke attempted", async () => {
+        try {
+          await instance.poke(accounts[0], { from: accounts[0] });
+          throw null;
+        } 
+        catch(error) {
+            assert(error, "Expected an error but did not receive one.");
+        }
+        
+    })
 });
